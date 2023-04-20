ad6df6540a8f77c849c5e5c0f309324c9c3d1a94
Zzhiwei <zhiwei3141@gmail.com>
2023-04-20 17:58:14 +0800

    fin iter, except, control flow, triangle_2, blocks

--- triangle.rb
+++ triangle.rb
@@ -13,8 +13,30 @@
 # and
 #   about_triangle_project_2.rb
 #
+def raiseIllegalTriangeError
+  raise TriangleError, "illegal triangle given"
+end
+
+
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  # any 0, any negative
+  if a <= 0 || b <= 0 || c <= 0
+    raiseIllegalTriangeError 
+  end
+  # sum of 2 smaller is <= sum of max
+  biggest = [a,b,c].max
+  other_1, other_2 = [a,b,c].min(2)
+  if other_1 + other_2 <= biggest
+    raiseIllegalTriangeError
+  end
+
+  if a == b && b == c
+    :equilateral
+  elsif a == b || a == c || b == c
+    :isosceles
+  else
+    :scalene
+  end 
 end
 
 # Error class used in part 2.  No need to change this code.
